a48c4c6507a9e9189be1376ced41fda1ac58dbf4
Dany1348 <mirnaleyla2020@gmail.com>
2025-11-27 21:36:17 -0300

    Se mueve de lugar la imagen del logo tiger a la carpete public y se utiliza como root

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from './Nav';
 import NavBar from './NavbarToggle';
+//import tigre from '../../public/tiger-feline-animal-image-pixabay-16.png'
 
 function Header() {
     return (
@@ -10,7 +11,10 @@ function Header() {
             
         }}>
             <h1>
-                <img src="../src/img/tiger-feline-animal-image-pixabay-16.png" title="Logo de Tiger" id="Logo" width="6%"  alt="img tigre"/>                          
+                {/* <img src="../src/img/tiger-feline-animal-image-pixabay-16.png" title="Logo de Tiger" id="Logo" width="6%"  alt="img tigre"/>                          
+                */}
+                 <img src="/tiger-feline-animal-image-pixabay-16.png" title="Logo de Tiger" id="Logo" width="6%"  alt="img tigre"/>                          
+               
                 Tienda Talento Tech React </h1>
             <Nav></Nav>
             <NavBar></NavBar>
